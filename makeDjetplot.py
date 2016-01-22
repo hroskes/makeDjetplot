cc9a4734c8067359610e182ec0fc571fc2135d80
Heshy Roskes <jroskes1@jhu.edu>
2016-01-22 19:24:37 +0100

    Style

--- makeDjetplot.py
+++ makeDjetplot.py
@@ -6,6 +6,11 @@ import style
 class Plot:
     maindir = "root://lxcms03://data3/Higgs/160111_ggZZincomplete/"
     basename = "ZZ4lAnalysis.root"
+    min = 0.
+    max = 1.
+    bins = 100
+    units = ""
+    normalizationincludesfailedevents = True
 
     def __init__(self, title, color, *CJLSTdirs):
         self.filenames = [os.path.join(self.maindir, dir, self.basename) for dir in CJLSTdirs]
@@ -23,25 +28,39 @@ class Plot:
             t.Add(filename)
         pvbf = array.array('f', [0])
         phjj = array.array('f', [0])
+        MC_weight = array.array('f', [1])
         t.SetBranchAddress("pvbf_VAJHU_old", pvbf)
         t.SetBranchAddress("phjj_VAJHU_old", phjj)
 
-        h = ROOT.TH1F("h"+self.title, "D_{jet}", 100, 0, 1)
+        h = ROOT.TH1F("h"+self.title, "D_{jet}", self.bins, self.min, self.max)
 
         length = t.GetEntries()
+        sumofweights = 0
         for i in range(length):
             t.GetEntry(i)
+
+            wt = MC_weight[0]
+
             if pvbf[0] >= 0 and phjj[0] >= 0 and not (pvbf[0] == phjj[0] == 0):
+
                 try:
                     Djet = (pvbf[0] / (pvbf[0] + phjj[0]))
                 except ZeroDivisionError:
                     t.Show(i)
                     raise
-                h.Fill(Djet)
+
+                h.Fill(Djet, wt)
+                sumofweights += wt
+
+            elif self.normalizationincludesfailedevents:
+                sumofweights += wt
+
             if (i+1) % 10000 == 0 or i+1 == length:
                 print (i+1), "/", length
 
         h.SetLineColor(self.color)
+        h.SetLineWidth(3)
+        h.Scale(1.0/sumofweights)
 
         self.__h = h
         return h
@@ -53,12 +72,19 @@ def makeDjetplots(*plots):
     c1 = ROOT.TCanvas()
     legend = ROOT.TLegend(0.6, 0.7, 0.9, 0.9)
     drawoption = ""
+    hstack = ROOT.THStack("hstack", "D_{jet}")
+    max, min, bins, units = None, None, None, None
     for plot in plots:
-        plot.h().Draw(drawoption)
-        if "same" not in drawoption: drawoption += "same"
+        hstack.Add(plot.h())
         plot.addtolegend(legend)
+        if max is None:
+            max, min, bins, units = plot.max, plot.min, plot.bins, plot.units
+        assert (max, min, bins, units) == (plot.max, plot.min, plot.bins, plot.units)
+    hstack.Draw("nostack")
+    hstack.GetXaxis().SetTitle("D_{jet}")
+    hstack.GetYaxis().SetTitle("fraction of events / %s%s" % ((max-min)/bins, " "+units if units else ""))
     legend.Draw()
-    c1.SaveAs("test.png")
+    c1.SaveAs("~/www/TEST/test.png")
 
 if __name__ == "__main__":
     plots = (
@@ -67,7 +93,7 @@ if __name__ == "__main__":
              Plot("ZH",   ROOT.kGreen-6,  "ZH125"),
              Plot("WH",   3,              "WplusH125", "WminusH125"),
              Plot("ttH",  4,              "ttH125"),
-             #Plot("qqZZ", 6,              "VBF125"),
+             #Plot("qqZZ", 6,              "???"),
              Plot("ggZZ", ROOT.kViolet-1, "ggZZ2e2mu", "ggZZ2e2tau", "ggZZ2mu2tau", "ggZZ4e", "ggZZ4mu", "ggZZ4tau"),
             )
     makeDjetplots(*plots)
